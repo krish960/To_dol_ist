b679315174bb2643f09d1f8bff6b4f124b68cc9b
krish960 <thombarek362@gmail.com>
2025-02-27 22:36:03 +0530

    secound commit

--- Supermarket/settings.py
+++ Supermarket/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'Supermarket.urls'
